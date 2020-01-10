a27e557da5ec9d8c1fe77f72821a98ad1e9e0509
Xavier Carty <xaviercarty@gmail.com>
2020-01-10 15:15:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ require "yaml"
 
 def load_library(file_path)
   # code goes here
-  emoticons = YAML.load_file(file_path)
+  emoticons = YAML.load(file_path)
 
   new_hash = {
     'get_meaning' => {},
