579aedd2968f5038384ad4524cc7809713ea5f81
Xavier Carty <xaviercarty@gmail.com>
2020-01-10 15:12:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,10 +9,8 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
 
   new_hash = {
-     "get_meaning" => {},
-     "get_emoticon" => {}
-
-
+     'get_meaning' => {},
+     'get_emoticon' => {}
   }
 
   emoticons.each do |key , value|
